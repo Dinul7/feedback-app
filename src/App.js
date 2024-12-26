31b13a3473859dd3073b367825ad75a6be255099
Dinul7 <131525087+Dinul7@users.noreply.github.com>
2024-12-26 15:52:33 +0200

    conditionals and fragments

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
+// Parte din proiect
 function App() {
-  return <h1>Hello from the app componenet</h1>;
+  return (
+    <div className="container">
+      <h1>My app</h1>
+    </div>
+  );
 }
+
+//learning things
+// function App() {
+//   const title = "Blog post";
+//   const body = "This is my blog post";
+//   const comments = [
+//     { id: 1, text: "Comment one" },
+//     { id: 2, text: "Comment two" },
+//     { id: 3, text: "Comment three" },
+//   ];
+
+//   const loading = false;
+
+//   const showComments = false;
+
+//   if (loading) return <h1>Loading...</h1>;
+
+//   const commentBlock = (
+//     <div className="comments">
+//       <h3>Comments ({comments.length})</h3>
+//       <ul>
+//         {comments.map((comment, index) => (
+//           <li key={index}>{comment.text}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+
+//   return (
+//     //fragment <></>
+//     <div className="container">
+//       <h1>{title.toUpperCase()}</h1>
+//       <p>{body}</p>
+
+//       {showComments && commentBlock}
+//     </div>
+//   );
+// }
 
 export default App;
