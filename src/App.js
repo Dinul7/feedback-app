e2ff8d128b81d7ec937cf122a62c752ae457d27d
Dinul7 <131525087+Dinul7@users.noreply.github.com>
2025-01-09 15:44:15 +0200

    Creating Routes and links

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
+// Api
+import { v4 as uuidv4 } from "uuid";
+
+// React properties
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from "react-router-dom";
 import { useState } from "react";
+
+// Components
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
-// import Card from "./components/shared/Card";
+import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
+
+import Card from "./components/shared/Card";
+import Post from "./components/Post";
 
 function App() {
   const [feedback, setFeedBack] = useState(FeedbackData);
+  const addFeedBack = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedBack([newFeedback, ...feedback]);
+  };
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
       setFeedBack(feedback.filter((item) => item.id !== id));
     }
   };
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-        {/* <Card>Hello world</Card> */}
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedBack} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/post/:id/:name" element={<Post />} />
+        </Routes>
+
+        {/* <Card>
+          <NavLink to="/" activeClassName="active">
+            Home
+          </NavLink>
+          <NavLink to="/about" activeClassName="active">
+            HAbout
+          </NavLink>
+        </Card> */}
       </div>
-    </>
+      <AboutIconLink />
+    </Router>
   );
 }
 
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,7 +3,7 @@ import Card from "./shared/Card";
 import Button from "./shared/Button";
 import RatingSelect from "./RatingSelect";
 
-function FeedbackForm() {
+function FeedbackForm({ handleAdd }) {
   const [text, setText] = useState("");
   const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
@@ -23,12 +23,28 @@ function FeedbackForm() {
 
     setText(e.target.value);
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedBack = {
+        text,
+        rating,
+      };
+
+      handleAdd(newFeedBack);
+      setText("");
+    }
+  };
 
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would your rate service with us</h2>
-        <RatingSelect />
+        <RatingSelect
+          select={(rating) => {
+            setRating(rating);
+          }}
+        />
         <div className="input-group">
           <input
             onChange={handleTextChange}
